21bd7868fc98b892c0524890b63aacbc803f9050
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-08-18 14:49:07 +0300

    renew layout

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from "react-router-dom";
-import { persistor, store } from "redux/store";
-import { App } from "components/App/App";
-import { GlobalStyle } from "components/baseStyles/GlobalStyle";
-import { ThemeStatus } from "components/ThemeStatus/ThemeProvider";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { persistor, store } from 'redux/store';
+import { App } from 'components/App/App';
+import { GlobalStyle } from 'components/baseStyles/GlobalStyle';
+import { ThemeStatus } from 'components/ThemeStatus/ThemeProvider';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
-window.global = { BASE_URL: "https://animal-welfare-network.onrender.com/api" };
-// window.global = { BASE_URL: 'http://localhost:3030/api' };
+window.global = { BASE_URL: 'http://localhost:3030/api' };
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={"Loading"} persistor={persistor}>
+      <PersistGate loading={'Loading'} persistor={persistor}>
         <BrowserRouter basename="animal-welfare-network">
           <I18nextProvider i18n={i18n}>
             <ThemeStatus>
@@ -27,5 +26,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
