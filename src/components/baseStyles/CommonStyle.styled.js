e0cbaff2753b2b09fea315cf3f4f76195337c748
JuliaGolban <julia.j.shcherban@gmail.com>
2023-08-25 19:06:17 +0300

    add markup catalog

--- src/components/baseStyles/CommonStyle.styled.js
+++ src/components/baseStyles/CommonStyle.styled.js
@@ -27,7 +27,7 @@ const Container = styled.div`
   margin: 0 auto;
   padding: 0 10px;
 
-  @media screen and (min-width: ${theme.breakpoints.desktop}) {
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
     padding: 0 30px;
   }
 
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { SEO } from 'utils/SEO';
+import { fetchData } from '../services/APIservice';
+import { Catalog } from '../components/Catalog/Catalog';
+import { onLoading, onLoaded } from 'components/helpers/Loader/Loader';
+import { onFetchError } from 'components/helpers/Messages/NotifyMessages';
 
 const CatalogPage = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    (async function getData() {
+      setIsLoading(true);
+      try {
+        const { data } = await fetchData(`/catalog`);
+        setProducts(data);
+        if (!data) {
+          return onFetchError(t('Whoops, something went wrong'));
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [t]);
+
   return (
     <>
       <SEO title="HomeForest Catalog" description="Catalog of goods" />
-      <h3>CatalogPage</h3>
+      {isLoading ? onLoading() : onLoaded()}
+      {error && onFetchError(t('Whoops, something went wrong'))}
+      {products.length > 0 && !error && <Catalog products={products} />}
     </>
   );
 };
