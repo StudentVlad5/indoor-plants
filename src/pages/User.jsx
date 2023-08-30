284295159d4a076a38b64fe0d94cd54c0ecfceb4
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-08-30 23:17:59 +0300

    add buttomLine for userpage

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { UserComp } from 'components/UserComp/UserComp';
 import { SEO } from 'utils/SEO';
 import { useDispatch } from 'react-redux';
-import { cleanHeaderBottom } from 'redux/header_bottom/operation';
+import { addHeaderBottom } from 'redux/header_bottom/operation';
 import { useEffect } from 'react';
 
 const UserPage = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(cleanHeaderBottom());
+    dispatch(
+      addHeaderBottom({
+        headerBottom: 'addHeaderBottom',
+      }),
+    );
   }, []);
   return (
     <>
