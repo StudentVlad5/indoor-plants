6a1e4def81e2c7df7bac56330ea80370b8ec22ff
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-08-31 08:36:41 +0300

    onClick for logo in mobile menu

--- src/components/Header/Elements/menu/menu.styled.js
+++ src/components/Header/Elements/menu/menu.styled.js
@@ -18,13 +18,14 @@ const MobileMenuSection = styled.div`
   top: 0px;
   left: 0;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   z-index: 999;
   transform: translateX(100%);
   transition: transform 250ms ease-in-out;
   display: flex;
   flex-direction: column;
   background-color: ${theme.colors.white};
+  overflow: hidden;
 
   &.is-expanded {
     transform: translateX(0);
--- src/components/Header/MobileMenu/MobileMenu.styled.js
+++ src/components/Header/MobileMenu/MobileMenu.styled.js
@@ -1,6 +1,7 @@
 import { ReactComponent as iconClose } from 'images/svg/icon_close.svg';
 import theme from 'components/baseStyles/Variables.styled';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const MobileHeader = styled.header`
   width: 100%;
@@ -27,4 +28,20 @@ const IconClose = styled(iconClose)`
   }
 `;
 
-export { MobileHeader, IconClose };
+const Text = styled(Link)`
+  font-family: ${theme.fonts[0]};
+  font-weight: 700;
+  font-style: normal;
+  font-size: ${theme.fontSizes.extraXL};
+  line-height: 42px;
+  letter-spacing: 0.07em;
+  color: ${theme.colors.brown2};
+  text-shadow: 2px 4px 2px rgba(0, 0, 0, 0.2);
+  text-decoration: none;
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    line-height: 48px;
+    font-size: ${theme.fontSizes.extraXXL};
+  }
+`;
+export { MobileHeader, IconClose, Text };
