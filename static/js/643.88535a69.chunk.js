b64094d6788068f852876b865d6756ad90622d75
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-06 19:39:57 +0000

    Deploying to gh-pages from @ StudentVlad5/indoor-plants@40bd5a5340d488fc31718dd42df022381ed86b6e 🚀

--- static/js/643.88535a69.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkindoor_plants=self.webpackChunkindoor_plants||[]).push([[643],{9643:function(e,t,n){n.r(t);var o=n(2791),r=n(4116),s=n(9434),i=n(456),a=n(3329);t.default=function(){var e=(0,s.I0)();return(0,o.useEffect)((function(){e((0,i.T)())}),[]),(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(r.H,{title:"HomeForest Favorite",description:"Favorite of goods"}),(0,a.jsx)("h3",{children:"FavoritePage"})]})}}}]);
-//# sourceMappingURL=643.88535a69.chunk.js.map
