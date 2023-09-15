8aaae99638402c753c380529f3bc389dee2ee7fc
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-15 16:08:13 +0300

    change styles in userform

--- src/components/UserComp/UserData/UserData.styled.js
+++ src/components/UserComp/UserData/UserData.styled.js
@@ -1,5 +1,6 @@
 import theme from 'components/baseStyles/Variables.styled';
 import { Button } from 'components/helpers/ButtonSplit/ButtonSplit.styled';
+import { ReactComponent as Pencil } from 'images/svg/pencil.svg';
 import styled from 'styled-components';
 
 const UserDataSection = styled.div`
@@ -31,7 +32,6 @@ const UserDataContainer = styled.div`
   }
 
   @media screen and (min-width: ${theme.breakpoints.desktop}) {
-    flex-direction: row;
     margin: 30px 0;
     padding: 0 20px;
   }
@@ -45,7 +45,7 @@ const UserDataImgWrapper = styled.div`
   }
   @media screen and (min-width: ${theme.breakpoints.desktop}) {
     position: relative;
-    margin-bottom: 36px;
+    margin-bottom: 90px;
   }
 `;
 
@@ -158,6 +158,133 @@ const BtnChangePassword = styled(Button)`
     background-color: ${theme.colors.green};
   }
 `;
+
+const BtnSave = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 4px;
+  color: ${theme.colors.brown1};
+  background: ${theme.colors.green4};
+  transform: scale(1);
+  cursor: pointer;
+  position: relative;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.25s ease-in;
+  :hover,
+  :focus {
+    transform: scale(1.05);
+    transition: transform 0.5s;
+    color: ${theme.colors.white};
+    background: ${theme.colors.brown2};
+  }
+  :disabled {
+    opacity: 0.5;
+    cursor: auto;
+    transform: none;
+    transition: none;
+  }
+`;
+
+const BtnCancel = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${theme.colors.brown};
+  border-radius: 4px;
+  color: ${theme.colors.brown1};
+  transform: scale(1);
+  cursor: pointer;
+  position: relative;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.25s ease-in;
+  :hover,
+  :focus {
+    transform: scale(1.05);
+    transition: transform 0.5s;
+  }
+  :disabled {
+    opacity: 0.5;
+    cursor: auto;
+    transform: none;
+    transition: none;
+  }
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  position: relative;
+  flex-direction: column;
+  padding: 20px 20px;
+  background-color: ${theme.colors.blue3};
+  width: 100%;
+  margin: 0 40px;
+`;
+
+const BtnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 20px;
+  width: 100%;
+  gap: 20px;
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    flex-direction: row;
+  }
+`;
+
+const ProfileSpanName = styled.span`
+  font-family: ${theme.fonts[1]};
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 0.32px;
+  color: ${theme.colors.green};
+`;
+
+const ProfileSpanValues = styled.span`
+  font-family: ${theme.fonts[0]};
+  font-size: ${theme.fontSizes.small};
+  font-style: normal;
+  font-weight: 400;
+  line-height: 140%;
+  margin: 4px 0;
+  color: ${theme.colors.brown1};
+`;
+
+const PensilStyle = styled(Pencil)`
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  width: 14px;
+  height: 14px;
+  fill: ${theme.colors.brown1};
+  transform: all 150ms linear;
+  cursor: pointer;
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+const TitleArticle = styled.div`
+  width: 100%;
+  padding: 0 20px;
+  font-family: ${theme.fonts[0]};
+  font-size: ${theme.fontSizes.medium};
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  color: ${theme.colors.brown1};
+`;
+
 export {
   EditCameraForm,
   EditCameraWrapper,
@@ -168,6 +295,14 @@ export {
   UserDataImgWrapper,
   UserDataList,
   BtnChangePassword,
+  BtnSave,
+  BtnCancel,
   UserDataSection,
   UserPasswordList,
+  ProfileContainer,
+  ProfileSpanName,
+  ProfileSpanValues,
+  PensilStyle,
+  TitleArticle,
+  BtnContainer,
 };
