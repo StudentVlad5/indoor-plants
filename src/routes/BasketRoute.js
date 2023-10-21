1300f3f793fd0a418306e578bba44c4676ee97b9
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-10-21 18:41:51 +0300

    fix BasketRoute

--- src/routes/BasketRoute.js
+++ src/routes/BasketRoute.js
@@ -1,37 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
-import { selectBasket } from 'redux/basket/selectors';
 import PropTypes from 'prop-types';
 import { getFromStorage } from 'services/localStorService';
-// import { useSelector } from 'react-redux';
+import { StatusContext } from 'components/ContextStatus/ContextStatus';
 
 export const BasketRoute = ({ component: Component, redirectTo = '/' }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [userAnonimusID, _] = useState(
     getFromStorage('userAnonimusID') ? getFromStorage('userAnonimusID') : '',
   );
-  const [basket, setBasket] = useState([]);
-
-  useEffect(() => {
-    (async function getItem() {
-      setIsLoading(true);
-      try {
-        const { data } = await getItemInBasket(`/basket/${userAnonimusID}`);
-        if (!data) {
-          return onFetchError(t('Whoops, something went wrong'));
-        }
-        setBasket(data.data);
-        saveToStorage('basketData', data.data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    })();
-  }, []);
-
-  const shouldRedirect = !basket || basket?.length === 0;
+  const { contextBasket, setContextBasket } = useContext(StatusContext);
+  const shouldRedirect =
+    !contextBasket || contextBasket[0]?.optionData?.length === 0;
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
 
